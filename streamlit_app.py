50587a4178fdf009e3cc6d931154e4446d89bcc8
KhaterPCRB <akhater@pcrb.com>
2025-03-19 15:37:24 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,9 +29,9 @@ if ingredients_list:
         s_o= pd_df.loc[pd_df['FRUIT_NAME'] == ing,'SEARCH_ON'].iloc[0]
         smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + s_o)
         if ingredients == '':
-            ingredients+= s_o
+            ingredients+= ing
         else:
-            ingredients= ingredients +", " + s_o
+            ingredients= ingredients +", " + ing
     
     
     #st.write(f'Search Value for {ing} is {s_o}.')
