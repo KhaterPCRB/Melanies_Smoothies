08f5cd638cd7f3fd5cf3306d7bf8f2a8287de50d
KhaterPCRB <akhater@pcrb.com>
2025-03-19 09:49:57 -0400

    Update streamlit_app.py
    Fixed Connection

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,8 +20,9 @@ st.title(":cup_with_straw: Customize Your Smoothie! :cup_with_straw:")
 # )   
 
 # st.write(f"You selected: {Coption} ")
-
-session = get_active_session()
+cnx= st.connect("snowflake")
+session = cnx.session()
+#session = get_active_session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 st.dataframe(data=my_dataframe, use_container_width=True)
 name=st.text_input("Enter the Name for the Order")
@@ -51,5 +52,4 @@ if ingredients_list:
         st.success(f'Smoothie Ordered, {name}')
 
 #===================
-cnx= st.connect("snowflake")
-session = cnx.session()
+
