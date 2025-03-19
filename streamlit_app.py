8714084fe73e59a706c6d2558baea6593d9aa10a
KhaterPCRB <akhater@pcrb.com>
2025-03-19 14:51:44 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -31,7 +31,7 @@ if ingredients_list:
         else:
             ingredients= ingredients +", " + ing
     smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" +ing)
-    s_o= pd_df.loc[pd_df['FRUIT_NAME'] == ing,'SEARCH_ON'].iloc[0]]
+    s_o= pd_df.loc[pd_df['FRUIT_NAME'] == ing,'SEARCH_ON'.iloc[0]]
     st.write(f'Search Value for {ing} is {s_o}.')
     st.subheader(ing + 'Nutriton Information')
     
